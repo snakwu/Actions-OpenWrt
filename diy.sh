81e4c291e274526a6125e0784afb7c26444b51d7
wctliu <wctliu@gmail.com>
2019-12-01 00:25:58 +0800

    update source

--- diy.sh
+++ diy.sh
@@ -11,7 +11,7 @@ git clone https://github.com/wctliu/lean.git package/
 git clone https://github.com/Ameykyl/my package/my
 git clone https://github.com/Ameykyl/luci-app-ssr-plus-jo package/luci-app-ssr-plus-jo
 sleep 40
-sed -i "/CYXluq4wUazHjmCDBCqXF/d" /package/lean/default-settings/files/zzz-default-settings
-sed -i "/DISTRIB_REVISION/d" /package/lean/default-settings/files/zzz-default-settings
-sed -i "/DISTRIB_DESCRIPTION/d" /package/lean/default-settings/files/zzz-default-settings
-sed -i "s/192\.168\.1\.1/192\.168\.188\.1/g" /package/base-files/files/bin/config_generate
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+sed -i "/DISTRIB_REVISION/d" package/lean/default-settings/files/zzz-default-settings
+sed -i "/DISTRIB_DESCRIPTION/d" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/192\.168\.1\.1/192\.168\.188\.1/g" package/base-files/files/bin/config_generate
