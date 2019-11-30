7e2b0adf4f79afee72b8d82dfc4d0ab45e4bfd03
wctliu <wctliu@gmail.com>
2019-12-01 00:56:04 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,12 +7,12 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-git clone https://github.com/wctliu/lean.git package/
-git clone https://github.com/Ameykyl/my package/my
-git clone https://github.com/Ameykyl/luci-app-ssr-plus-jo package/luci-app-ssr-plus-jo
+git clone https://github.com/wctliu/lean.git package/lean/
+git clone https://github.com/Ameykyl/my package/my/
+git clone https://github.com/Ameykyl/luci-app-ssr-plus-jo package/luci-app-ssr-plus-jo/
 sleep 40
 ./scripts/feeds update -a && ./scripts/feeds install -a
-sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
-sed -i "/DISTRIB_REVISION/d" package/lean/default-settings/files/zzz-default-settings
-sed -i "/DISTRIB_DESCRIPTION/d" package/lean/default-settings/files/zzz-default-settings
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/lean/default-settings/files/zzz-default-settings
+sed -i "/DISTRIB_REVISION/d" package/lean/lean/default-settings/files/zzz-default-settings
+sed -i "/DISTRIB_DESCRIPTION/d" package/lean/lean/default-settings/files/zzz-default-settings
 sed -i "s/192\.168\.1\.1/192\.168\.188\.1/g" package/base-files/files/bin/config_generate
