615a1ef810c29c067748efe2d13e89c37794af05
wctliu <wctliu@gmail.com>
2019-11-30 19:32:09 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,7 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" /home/snakwu/openwrt-master/openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i "/DISTRIB_REVISION/d" /home/snakwu/openwrt-master/openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i "/DISTRIB_DESCRIPTION/d" /home/snakwu/openwrt-master/openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i "s/192\.168\.1\.1/192\.168\.188\.1/g" /home/snakwu/openwrt-master/openwrt/package/base-files/files/bin/config_generate
