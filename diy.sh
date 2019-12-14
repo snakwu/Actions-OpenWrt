ecab850ab9a6b03ef96b209ab39c8a820c2034f2
wctliu <wctliu@gmail.com>
2019-12-14 18:41:42 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,7 +9,7 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/wctliu/lean.git package/lean/
 git clone https://github.com/Ameykyl/my package/my/
-git clone https://github.com/Ameykyl/luci-app-ssr-plus-jo package/luci-app-ssr-plus-jo/
+git clone https://github.com/Leo-Jo/luci-app-ssr-plus-jo.git package/luci-app-ssr-plus-jo/
 sleep 40
 ./scripts/feeds update -a && ./scripts/feeds install -a
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/lean/default-settings/files/zzz-default-settings
