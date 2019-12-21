0c06eefcd9c692138f21c07ffb6e75ecfc25fc73
SnakWu <snakwu@gmail.com>
2019-12-21 23:03:29 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -15,4 +15,4 @@ sleep 40
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/lean/default-settings/files/zzz-default-settings
 sed -i "/DISTRIB_REVISION/d" package/lean/lean/default-settings/files/zzz-default-settings
 sed -i "/DISTRIB_DESCRIPTION/d" package/lean/lean/default-settings/files/zzz-default-settings
-sed -i "s/192\.168\.1\.1/192\.168\.188\.1/g" package/base-files/files/bin/config_generate
+sed -i "s/192\.168\.1\.1/192\.168\.88\.1/g" package/base-files/files/bin/config_generate
